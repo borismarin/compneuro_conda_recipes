f92ecd010f7f2c5b4d03c89d411038f862fce2ca
Boris Marin <borismarin@gmail.com>
2015-01-03 15:16:20 -0200

    organizing nonstandard dirs created by genesis

--- genesis/build.sh
+++ genesis/build.sh
@@ -1,4 +1,9 @@
+#make install generates non-standard directories under _prefix_
+# will dump everything into opt/genesis and copy bin/ man/ to $PREFIX at
+# post-link.sh
+FULL_PREFIX=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 cd src
-./configure --prefix=$PREFIX
+./configure --prefix=$FULL_PREFIX
 make
-make install INSTALLDIR=$PREFIX
+make install  
+
--- genesis/post-link.sh
+++ genesis/post-link.sh
@@ -1,2 +1,13 @@
+FULL_PREFIX=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+
+#bin
+cp $FULL_PREFIX/genesis $PREFIX/bin/
+cp $FULL_PREFIX/src/convert/convert $PREFIX/bin/genesis_convert
+
+#man
+mkdir -p $PREFIX/share/man/man1
+cp $FULL_PREFIX/man/man1/convert.1 $PREFIX/share/man/man1/genesis_convert.1
+
+
 echo "Creating / updating the simrc file"
-cp -i $PREFIX/src/startup/simrc $HOME/.simrc
+cp -i $FULL_PREFIX/.simrc $HOME/
