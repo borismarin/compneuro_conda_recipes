a9740baddecdbea4ca471fefdb242a94c585b8f9
Boris Marin <borismarin@gmail.com>
2015-01-03 19:58:18 +0000

    logic to fix netcdf compilation on osx

--- genesis/build.sh
+++ genesis/build.sh
@@ -1,7 +1,15 @@
+# Circumvent netcdf compilation problem when arch is set on OSX
+if [ `uname` == Darwin ]; then
+    for x in OSX_ARCH CFLAGS CXXFLAGS LDFLAGS
+    do
+        unset $x
+    done
+fi
+FULL_PREFIX=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
+
 #make install generates non-standard directories under _prefix_
 # will dump everything into opt/genesis and copy bin/ man/ to $PREFIX at
 # post-link.sh
-FULL_PREFIX=$PREFIX/opt/$PKG_NAME-$PKG_VERSION
 cd src
 ./configure --prefix=$FULL_PREFIX
 make
