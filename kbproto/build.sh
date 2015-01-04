bf31ab61117132c3eac3d4da59b01f8d42dc3858
Boris Marin <borismarin@gmail.com>
2015-01-04 05:54:41 +0000

    some dependencies and fixes for building libx11 on osx

--- kbproto/build.sh
+++ kbproto/build.sh
@@ -1,5 +1,6 @@
 export ACLOCAL="aclocal -I $PREFIX/share/aclocal"
-export PKG_CONFIG_PATH="$PREFIX/share/pkgconfig:$PREFIX/lib/pkgconfig"
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+
 ./autogen.sh
 ./configure --prefix=$PREFIX 
 make
